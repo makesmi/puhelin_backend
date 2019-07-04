8f9d313b4540172e3d7b2da30fa9e8b529ebbd6a
Markus Mustonen <makesmi@hotmail.com>
2019-07-04 16:02:03 +0300

    uibuild

---
+++ build/precache-manifest.73ed613092da06af089ba3c539e1cd75.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e904d4aa9c6beda5199e1bcbd25ef4d8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6e3df25ed076df1d823c",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "bc120ca277fc438795d3",
+    "url": "/static/js/2.dbf5eb8f.chunk.js"
+  },
+  {
+    "revision": "6e3df25ed076df1d823c",
+    "url": "/static/js/main.b2ca3f94.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- index.js
+++ index.js
@@ -9,6 +9,7 @@ morgan.token('body', (req, res) => req.method === 'POST' && JSON.stringify(req.b
 
 app.use(cors())
 app.use(bodyParser.json())
+app.use(express.static('build'))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 
