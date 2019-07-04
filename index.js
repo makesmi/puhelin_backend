a05bddce15efafdf66476c176ec0971b48bbdbb9
Markus Mustonen <makesmi@hotmail.com>
2019-07-04 15:05:56 +0300

    lisäys: tehtävä 3.9 puhelinluettelon backend step 9

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
+const cors = require('cors') 
 
 const app = express()
 
 morgan.token('body', (req, res) => req.method === 'POST' && JSON.stringify(req.body))
 
+app.use(cors())
 app.use(bodyParser.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
